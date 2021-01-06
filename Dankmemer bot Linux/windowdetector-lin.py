a10095b020db1fb0264312e1237746bfe713e6db
Rayr <ramtin.015@gmail.com>
2021-01-06 16:05:23 +0330

    Add files via upload

---
+++ Dankmemer bot Linux/windowdetector-lin.py
@@ -0,0 +1,26 @@
+import os
+import time
+import termcolor
+import colorama
+colorama.init()
+#from win32gui import GetWindowText, GetForegroundWindow
+#linux dependencies
+import gi
+gi.require_version("Wnck", "3.0")
+from gi.repository import Wnck
+
+print('Waiting... 5 seconds ', end='')
+print(termcolor.colored('Please click on Discord', 'green',))
+time.sleep(5)
+os.system('clear')
+print(' ')
+print(' ')
+print('Focused Window:')
+print(' ')
+#x = (GetWindowText(GetForegroundWindow()))
+scr = Wnck.Screen.get_default()
+scr.force_update()
+x = (scr.get_active_window().get_name())
+print('          ' + termcolor.colored(x, 'grey', 'on_white'))
+print(' ')
+print('     ' + termcolor.colored('↑ Paste this if the script asks for the window name ↑', 'blue',))
