d8614ed75250ceb329cea60d37db64fde0664616
Rayr <25773909+Rayrsn@users.noreply.github.com>
2021-07-13 00:18:08 +0430

    Fixed the linux bug

--- Dankmemer bot Linux/windowdetector-lin.py
+++ Dankmemer bot Linux/windowdetector-lin.py
@@ -22,6 +22,7 @@ colorama.init()
 import gi
 gi.require_version("Wnck", "3.0")
 from gi.repository import Wnck
+from gi.repository import Gtk
 
 print('Waiting... 5 seconds ', end='')
 print(termcolor.colored('Please click on Discord', 'green',))
@@ -32,9 +33,9 @@ print(' ')
 print('Focused Window:')
 print(' ')
 #x = (GetWindowText(GetForegroundWindow()))
-scr = Wnck.Screen.get_default()
-scr.force_update()
-x = (scr.get_active_window().get_name())
+screen = Wnck.Screen.get_default()
+screen.force_update()
+x = (screen.get_active_window().get_name())
 print('          ' + termcolor.colored(x, 'grey', 'on_white'))
 print(' ')
 print('     ' + termcolor.colored('↑ Paste this if the script asks for the window name ↑', 'blue',))
